4bff6f87d74e6eea3c6e5f2c20307530126ca2f3
speedyjones@yahoo.com <Prabeerk007>
2022-04-09 04:59:06 +0530

    Basic ATM System. V 1.0.0

---
+++ src/JavaBankingApplication/JavaBankingApplication.java
@@ -0,0 +1,124 @@
+package JavaBankingApplication;
+
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class JavaBankingApplication {
+
+    static final Logger logs = Logger.getLogger(String.valueOf(JavaBankingApplication.class));
+    String msg = "Please Collect Your ";
+    String notesMsg = " Notes ";
+
+    long amtTotal = 0;
+    long total = 0;
+    long qty = 0;
+
+    int oneHundredQty = 10;
+    int twoHundredQty = 10;
+    int fiveHundredQty = 10;
+
+    int amtCount = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        logs.info("Welcome to ABC Bank....");
+        logs.info("Please Insert Your Card & Procced With Your Transcation !!!");
+
+        Scanner cardDetect = new Scanner(System.in);
+        int cardDetectValue = cardDetect.nextInt();
+
+        logs.info("--------------------------------------------");
+
+        if (cardDetectValue == 1) {
+            logs.info("Please Wait .......");
+            Thread.sleep(1000);
+            JavaBankingApplication jba = new JavaBankingApplication();
+            jba.pinValidate();
+
+
+        } else {
+            logs.info("Wrong Input !!!");
+        }
+
+    }
+
+    public int pinValidate() throws InterruptedException {
+
+
+        logs.info("Please Enter Your PIN !!!!");
+        logs.info("PIN : ");
+        Scanner pinVal = new Scanner(System.in);
+        int pinReceived = pinVal.nextInt();
+        String pinLength = Integer.toString(pinReceived);
+
+
+        if (pinLength.length() == 4) {
+            logs.info("Please Wait .......");
+            Thread.sleep(1000);
+            amount();
+        } else {
+            logs.info("Invalidate Pin, Please Validate Again !!!!");
+
+        }
+
+        return pinReceived;
+    }
+
+    public long amount() {
+
+
+        logs.fine("Enter The Amount, When You're Ready !!!!");
+        logs.info("Amount : ");
+        Scanner amountInput = new Scanner(System.in);
+        long amtInput = amountInput.nextInt();
+
+        calAmount(amtInput);
+        return amtInput;
+
+    }
+
+    public long calAmount(long amtInput) {
+
+
+        if (amtInput < 100) {
+            logs.warning("Please Enter Amount Above 100 !!!!");
+
+        } else {
+            while (amtInput > 99) {
+                if (amtInput >= 100 && amtInput < 200) {
+                    amtCount = 100;
+                    total = amtInput / amtCount;
+                    amtTotal = amtInput - (total * amtCount);
+                } else if (amtInput >= 200 && amtInput < 500) {
+                    amtCount = 200;
+                    total = amtInput / amtCount;
+                    amtTotal = amtInput - (total * amtCount);
+
+                } else if (amtInput >= 500 && amtInput < 2000) {
+                    amtCount = 500;
+                    total = amtInput / amtCount;
+                    amtTotal = amtInput - (total * amtCount);
+
+                } else if (amtInput >= 2000) {
+                    amtCount = 2000;
+                    total = amtInput / amtCount;
+
+                    amtTotal = amtInput - (total * amtCount);
+                }
+                String finalMsg = msg + total + notesMsg + amtCount + "'s";
+                logs.log(Level.INFO,finalMsg);
+
+                amtInput = amtTotal;
+
+            }
+
+        }
+
+
+        return amtInput;
+    }
+
+
+}
